33ce87c4b5b3913d558ed9ff47d0e98661591ac1
寰宇体 <3591079292@qq.com>
2022-08-09 18:49:40 +0800

    script/canvas

---
+++ script/Home.js
@@ -0,0 +1,35 @@
+           const text = document.querySelector('.text');
+            const txt  =["嵌入式系统训练基地","欢迎你的加入",]; 
+            
+            var index=0;
+            var xiaBiao= 0;
+            var huan = true;
+         
+            setInterval(function(){
+               
+                if(huan){      
+                    text.innerHTML = txt[xiaBiao].slice(0,++index);
+                    console.log(index);
+                }
+                else{
+                    text.innerHTML = txt[xiaBiao].slice(0,index--);
+                    console.log(index);
+                }
+    
+                if(index==txt[xiaBiao].length+3)
+                {
+                    huan = false;
+                }
+                else if(index<0)
+                {
+                    index = 0;
+                    huan = true;
+                    xiaBiao++;
+                    if(xiaBiao>=txt.length)
+                    {
+                        xiaBiao=0; 
+                    }
+                }
+    
+            },200)
+    
